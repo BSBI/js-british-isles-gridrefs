0eefb181b4b5dc09703c516e19812aa2c61d0375
Tom Humphrey <tomhumphrey@gmail.com>
2021-11-27 19:01:57 +0000

    again

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 // rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -25,6 +26,7 @@ export default {
       exclude: 'node_modules/**', // only transpile our source code
       babelHelpers: 'runtime'
     }),
+    commonjs(), // converts npm packages to ES modules
     production && terser() // minify, but only in production
 	//(process.env.NODE_ENV === 'production' && uglify())
 	//uglify()
